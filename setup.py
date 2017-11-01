3f8bdd93f225c2950f4c21ce0336cd1b3344f768
harisbal <hbsolutionsgr@hotmail.com>
2017-11-01 11:30:40 +0000

    Remove matsimpy - Included analysis notebooks

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transpy',
+    version='0.1dev',
+    packages=find_packages(),
+    license='GNU Lesser General Public License v3.0',
+    long_description=open('README.md').read(),
+)
